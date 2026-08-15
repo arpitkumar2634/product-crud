66091a3af67e4b20673713757cc31ca49916876a
arpitkumar2634 <arpitkumarkts6400@gmail.com>
2026-08-15 14:27:03 +0530

    Test correct GitHub email

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -14,7 +14,7 @@
      
 @if ($errors->any())
     <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with inputs.<br><br>
+        <strong>Whoops!</strong> There were some problems with input.<br><br>
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
